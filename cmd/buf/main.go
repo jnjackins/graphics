b971184620d227da94965c05650eb3bd9e8c3f4d
Jeremy Jackins <jeremyjackins@gmail.com>
2015-02-23 16:14:15 +0900

    cmd/buf: read and write files

--- cmd/buf/main.go
+++ cmd/buf/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"image"
+	"io/ioutil"
 	"os"
 	"runtime"
 
@@ -29,15 +32,44 @@ func (e mouseEvent) Buttons() int {
 }
 
 func main() {
-	var err error
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	flag.Parse()
+	inputfile := false
+	if len(flag.Args()) == 1 {
+		inputfile = true
+	} else if len(flag.Args()) > 1 {
+		fmt.Fprintln(os.Stderr, "Usage: buf [file]")
+		os.Exit(1)
+	}
+
 	gopath := os.Getenv("GOPATH")
 	fontpath := gopath + "/src/github.com/jnjackins/graphics/cmd/buf/proggyfont.ttf"
-	buf, err = text.NewBuffer(image.Rect(0, 0, 300, 300), fontpath)
+	var err error
+	buf, err = text.NewBuffer(image.Rect(0, 0, 800, 600), fontpath)
 	die.On(err)
-	buf.LoadString("test")
-	buf.Select(text.Address{0, 0}, text.Address{0, 0})
-	disp, err = draw.Init(nil, "buf", "300x322")
+
+	var path string
+	if inputfile {
+		path = flag.Arg(0)
+		_, err := os.Stat(path)
+		if err != nil {
+			// if there's no file, no worries. otherwise, bail.
+			if !os.IsNotExist(err) {
+				die.On(err)
+			}
+		} else {
+			// no issues, open file for reading
+			f, err := os.Open(path)
+			die.On(err)
+			s, err := ioutil.ReadAll(f)
+			buf.LoadString(string(s))
+			buf.Select(text.Selection{})
+			f.Close()
+		}
+	}
+
+	disp, err = draw.Init(nil, "buf", "800x622")
 	die.On(err)
 	defer disp.Close()
 	screen = disp.ScreenImage
@@ -55,6 +87,9 @@ loop:
 		case ke := <-kbd.C:
 			// esc
 			if ke == 27 {
+				if path != "" {
+					ioutil.WriteFile(path, []byte(buf.Contents()), 0666)
+				}
 				break loop
 			}
 			buf.SendKey(ke)
