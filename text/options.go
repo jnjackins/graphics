b022d27ffa33e0db6c1e0dbc61e83ec514c51cf3
Jeremy Jackins <jeremyjackins@gmail.com>
2015-02-25 17:04:04 +0900

    text: scroll by sweeping past the edge of the window

--- text/options.go
+++ text/options.go
@@ -21,7 +21,7 @@ var AcmeTheme = OptionSet{
 		bgCol := image.NewUniform(color.RGBA{R: 0xFF, G: 0xFF, B: 0xEA, A: 0xFF})
 		cursor := image.NewRGBA(image.Rect(0, 0, 3, height))
 		draw.Draw(cursor, cursor.Rect, image.Black, image.ZP, draw.Src)
-		h := height - 3 // TODO magic 3
+		h := height - 3
 		draw.Draw(cursor, image.Rect(0, 3, 1, h), bgCol, image.ZP, draw.Src)
 		draw.Draw(cursor, image.Rect(2, 3, 3, h), bgCol, image.ZP, draw.Src)
 		return cursor
