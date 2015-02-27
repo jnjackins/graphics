3fb3272cc002aa871ae38eee3515289d36d42f88
Jeremy Jackins <jeremyjackins@gmail.com>
2015-02-27 09:09:21 +0900

    cmd/buf: update imports and use new clipboard functionality in text package

--- cmd/buf/main.go
+++ cmd/buf/main.go
@@ -5,13 +5,19 @@ import (
 	"fmt"
 	"image"
 	"io/ioutil"
+	"log"
 	"os"
 	"runtime"
 	"runtime/pprof"
 
-	"code.google.com/p/jnj.plan9/draw"
-	"code.google.com/p/jnj/die"
+	"github.com/jnjackins/die"
 	"github.com/jnjackins/graphics/text"
+	"github.com/jnjackins/plan9/draw"
+)
+
+const (
+	width  = 800
+	height = 600
 )
 
 var (
@@ -21,6 +27,25 @@ var (
 )
 
 var cpuprofile = flag.String("cpuprofile", "", "provide a path for cpu profile")
+
+type snarfer struct {
+	d *draw.Display
+}
+
+func (sn snarfer) Get() string {
+	b, err := sn.d.ReadSnarf()
+	if err != nil {
+		log.Println("buf: error reading from snarf buffer: " + err.Error())
+	}
+	return string(b)
+}
+
+func (sn snarfer) Put(s string) {
+	err := sn.d.WriteSnarf([]byte(s))
+	if err != nil {
+		log.Println("buf: error writing to snarf buffer: " + err.Error())
+	}
+}
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -36,7 +61,7 @@ func main() {
 
 	if *cpuprofile != "" {
 		profileWriter, err := os.Create(*cpuprofile)
-		die.On(err, "error creating file for cpu profile")
+		die.On(err, "buf: error creating file for cpu profile")
 		defer profileWriter.Close()
 		pprof.StartCPUProfile(profileWriter)
 		defer pprof.StopCPUProfile()
@@ -45,8 +70,8 @@ func main() {
 	gopath := os.Getenv("GOPATH")
 	fontpath := gopath + "/src/github.com/jnjackins/graphics/cmd/buf/proggyfont.ttf"
 	var err error
-	buf, err = text.NewBuffer(image.Rect(0, 0, 800, 600), fontpath, text.AcmeTheme)
-	die.On(err)
+	buf, err = text.NewBuffer(image.Rect(0, 0, width, height), fontpath, text.AcmeTheme)
+	die.On(err, "buf: error creating new text buffer")
 
 	var path string
 	if inputfile {
@@ -55,12 +80,12 @@ func main() {
 		if err != nil {
 			// if there's no file, no worries. otherwise, bail.
 			if !os.IsNotExist(err) {
-				die.On(err)
+				die.On(err, "buf: error opening input file")
 			}
 		} else {
 			// no issues, open file for reading
 			f, err := os.Open(path)
-			die.On(err)
+			die.On(err, "buf: error opening input file")
 			s, err := ioutil.ReadAll(f)
 			buf.LoadString(string(s))
 			buf.Select(text.Selection{})
@@ -68,12 +93,13 @@ func main() {
 		}
 	}
 
-	disp, err = draw.Init(nil, "buf", "800x622")
-	die.On(err)
+	disp, err = draw.Init("buf", width, height)
+	die.On(err, "buf: error initializing display device")
 	defer disp.Close()
+	buf.Clipboard = snarfer{disp}
 	screen = disp.ScreenImage
-	mouse := disp.InitMouse()
 	kbd := disp.InitKeyboard()
+	mouse := disp.InitMouse()
 	redraw()
 
 loop:
@@ -83,11 +109,19 @@ loop:
 			resize()
 		case me := <-mouse.C:
 			buf.SendMouseEvent(me.Point, me.Buttons)
+
+			// if we are getting rapid-fire mouse events (scrolling or sweeping),
+			// batch up a bunch of them before redrawing.
+			for len(mouse.C) > 0 {
+				me = <-mouse.C
+				buf.SendMouseEvent(me.Point, me.Buttons)
+			}
 		case ke := <-kbd.C:
 			// esc
 			if ke == 27 {
 				if path != "" {
-					ioutil.WriteFile(path, []byte(buf.Contents()), 0666)
+					err := ioutil.WriteFile(path, []byte(buf.Contents()), 0666)
+					die.On(err, "buf: error writing to file")
 				}
 				break loop
 			}
@@ -101,13 +135,14 @@ loop:
 
 func redraw() {
 	img, clipr := buf.Img()
-	_, err := screen.Load(screen.Bounds(), img.SubImage(clipr).(*image.RGBA).Pix)
-	die.On(err)
+	pix := img.SubImage(clipr).(*image.RGBA).Pix
+	_, err := screen.Load(screen.Bounds(), pix)
+	die.On(err, "buf: error sending image bytes to display")
 	disp.Flush()
 }
 
 func resize() {
 	err := disp.Attach(draw.Refmesg)
-	die.On(err, "error reattaching display after resize")
+	die.On(err, "buf: error reattaching display after resize")
 	buf.Resize(screen.Bounds())
 }
