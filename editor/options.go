919b5e40d21f9c1d5008d959865e16324d69145f
Jeremy Jackins <jeremy.jackins@meltwater.com>
2016-12-22 09:54:47 -0700

    graphics/editor: fix acme cursor scaling and background color

--- editor/options.go
+++ editor/options.go
@@ -23,13 +23,22 @@ type OptionSet struct {
 	ScrollBar  bool
 }
 
-var acmeCursor = func(height int) image.Image {
-	bgcol := image.NewUniform(color.RGBA{R: 0xFF, G: 0xFF, B: 0xEA, A: 0xFF})
-	cursor := image.NewRGBA(image.Rect(0, 0, 6, height))
-	draw.Draw(cursor, cursor.Rect, image.Black, image.ZP, draw.Src)
-	draw.Draw(cursor, image.Rect(0, 6, 2, height-6), bgcol, image.ZP, draw.Src)
-	draw.Draw(cursor, image.Rect(4, 6, 6, height-6), bgcol, image.ZP, draw.Src)
-	return cursor
+func acmeCursor(bg image.Image) func(height int) image.Image {
+	fn := func(height int) image.Image {
+		// the squares at the top and bottom of the cursor should
+		// be 3x3 points, and the center bar should be one point.
+		// we don't have any DPI info (TODO), so try to come up with
+		// something reasonable based on height.
+		scale := height/(6*3) + 1
+
+		cursor := image.NewRGBA(image.Rect(0, 0, 3*scale, height))
+		draw.Draw(cursor, cursor.Rect, image.Black, image.ZP, draw.Src)
+		draw.Draw(cursor, image.Rect(0, 3*scale, 1*scale, height-(3*scale)), bg, image.ZP, draw.Src)
+		draw.Draw(cursor, image.Rect(2*scale, 3*scale, 3*scale, height-(3*scale)), bg, image.ZP, draw.Src)
+		return cursor
+	}
+
+	return fn
 }
 
 var AcmeYellowTheme = &OptionSet{
@@ -38,7 +47,7 @@ var AcmeYellowTheme = &OptionSet{
 	BG2:        image.NewUniform(color.RGBA{R: 0xA0, G: 0xA0, B: 0x4B, A: 0xFF}),
 	Sel:        image.NewUniform(color.RGBA{R: 0xEE, G: 0xEE, B: 0x9E, A: 0xFF}),
 	Margin:     image.Pt(4, 0),
-	Cursor:     acmeCursor,
+	Cursor:     acmeCursor(image.NewUniform(color.RGBA{R: 0xFF, G: 0xFF, B: 0xEA, A: 0xFF})),
 	AutoIndent: true,
 	ScrollBar:  true,
 }
@@ -49,7 +58,7 @@ var AcmeBlueTheme = &OptionSet{
 	BG2:        image.NewUniform(color.RGBA{R: 0x88, G: 0x88, B: 0xCC, A: 0xFF}),
 	Sel:        image.NewUniform(color.RGBA{R: 0x9F, G: 0xEB, B: 0xEA, A: 0xFF}),
 	Margin:     image.Pt(4, 0),
-	Cursor:     acmeCursor,
+	Cursor:     acmeCursor(image.NewUniform(color.RGBA{R: 0xEA, G: 0xFF, B: 0xFF, A: 0xFF})),
 	AutoIndent: true,
 	ScrollBar:  true,
 }
